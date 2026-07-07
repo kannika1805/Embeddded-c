0b2e59e8ab24352dbac50fc35b08a4271ec1fbbc
Kannika <k98788846@gmail.com>
2026-07-07 19:18:07 +0530

    Q2

---
+++ TrafficLed2.ino
@@ -0,0 +1,26 @@
+int led1=2;
+int led2=6;
+int led3=4;
+void setup() {
+
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+ pinMode(led2, OUTPUT);
+ pinMode(led3, OUTPUT);
+ 
+}
+
+void loop () {
+  // put your main coe here, to run repeatedly:
+    // red
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    delay(1000);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    delay(1000);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    delay(1000);
+}
